6b5737ff9deaf3baa4519e9ddb2b8dd77afcb2af
Daniel Kcomt <kcomtdaniel@gmail.com>
2024-12-07 11:52:48 -0500

    added t3gallery item

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,24 +3,37 @@ import "./portfolio.css";
 import HFIMGEN from "../../assets/portfolio/hg-image-generator.gif";
 import SIRVALO from "../../assets/portfolio/sirvalogames.gif";
 import PRESENTS from "../../assets/portfolio/presents.gif";
-// import DANCEAROUND from "../../assets/portfolio/dancearound.gif";
+import DANCEAROUND from "../../assets/portfolio/dancearound.gif";
+import T3GALLERY from "../../assets/portfolio/T3Gallery.gif";
 
-const data = [
-  // {
-  //   id: 1,
-  //   image: DANCEAROUND,
-  //   title: "Dance Around: Personal Project",
-  //   github: "https://github.com/dansaround/dancearound",
-  //   demo: "https://dancearound.vercel.app/",
-  //   description: "Personal video library using Youtube's API",
-  // },
+const portfolioItems = [
+  {
+    id: 1,
+    image: DANCEAROUND,
+    title: "Dance Around: Personal Project",
+    github: "https://github.com/dansaround/dancearound",
+    demo: "https://dancearound.vercel.app/",
+    description:
+      "Personal video library using Youtube's API. React + Javascript",
+  },
+  {
+    id: 0,
+    image: T3GALLERY,
+    title: "T3 Gallery",
+    github: "https://github.com/dansaround/gallery-t3",
+    demo: "https://gallery-t3-five.vercel.app",
+    description:
+      "T3 Stack with Drizzle, Clerk, UploadThing, Posthog. Next.js + Typescript",
+  },
+
   {
     id: 2,
     image: HFIMGEN,
     title: "AI Image Generator",
     github: "https://github.com/dansaround/hf-image-generator",
     demo: "https://hf-image-generator.vercel.app/",
-    description: "Image Generator using Huggingface.js Inference API.",
+    description:
+      "Image Generator using Huggingface.js Inference API. React + Typescript",
   },
   {
     id: 4,
@@ -29,7 +42,7 @@ const data = [
     github: "https://github.com/dansaround/presents",
     demo: "https://presents-lac.vercel.app/",
     description:
-      "Digital Hub for Real State Agents, think linktree but 10x better.",
+      "Digital Hub for Real State Agents, think linktree but 10x better. React + Typescript",
   },
   {
     id: 3,
@@ -38,7 +51,7 @@ const data = [
     github: "https://github.com/dansaround/hf-image-generator",
     demo: "https://www.instagram.com/sirvalo.games/",
     description:
-      "Fullstack app with Node, React Native and Firebase | Repos are Private: links will redirect to customer's site.",
+      "Fullstack app with Node, React Native and Firebase | Repos are Private: links will redirect to customer's site. Typescript",
   },
 ];
 
@@ -49,7 +62,7 @@ const Portfolio = () => {
       <h2>Portfolio </h2>
 
       <div className="container portfolio__container">
-        {data.reverse().map((item) => (
+        {portfolioItems.reverse().map((item) => (
           <article key={item.id} className="portfolio__item">
             <div className="portfolio__item-image">
               <img src={item.image} alt="portfolio item" />
