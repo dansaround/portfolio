1fba7631c200fec763205ac6635406d59237d718
Daniel Kcomt <kcomtdaniel@gmail.com>
2025-01-13 11:29:32 -0500

    added Anime2Watch project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,10 +5,11 @@ import SIRVALO from "../../assets/portfolio/sirvalogames.gif";
 import PRESENTS from "../../assets/portfolio/presents.gif";
 import DANCEAROUND from "../../assets/portfolio/dancearound.gif";
 import T3GALLERY from "../../assets/portfolio/T3Gallery.gif";
+import ANIME2WATCH from "../../assets/portfolio/Anime2Watch.gif";
 
 const portfolioItems = [
   {
-    id: 1,
+    id: 0,
     image: DANCEAROUND,
     title: "Dance Around: Personal Project",
     github: "https://github.com/dansaround/dancearound",
@@ -17,13 +18,13 @@ const portfolioItems = [
       "Personal video library using Youtube's API. React + Javascript",
   },
   {
-    id: 0,
-    image: T3GALLERY,
-    title: "T3 Gallery",
-    github: "https://github.com/dansaround/gallery-t3",
-    demo: "https://gallery-t3-five.vercel.app",
+    id: 1,
+    image: PRESENTS,
+    title: "Presents: Link Hub",
+    github: "https://github.com/dansaround/presents",
+    demo: "https://presents-lac.vercel.app/",
     description:
-      "T3 Stack with Drizzle, Clerk, UploadThing, Posthog. Next.js + Typescript",
+      "Digital Hub for Real State Agents, think linktree but 10x better. React + Typescript",
   },
 
   {
@@ -36,22 +37,31 @@ const portfolioItems = [
       "Image Generator using Huggingface.js Inference API. React + Typescript",
   },
   {
-    id: 4,
-    image: PRESENTS,
-    title: "Presents: Link Hub",
-    github: "https://github.com/dansaround/presents",
-    demo: "https://presents-lac.vercel.app/",
+    id: 3,
+    image: T3GALLERY,
+    title: "T3 Gallery",
+    github: "https://github.com/dansaround/gallery-t3",
+    demo: "https://gallery-t3-five.vercel.app",
     description:
-      "Digital Hub for Real State Agents, think linktree but 10x better. React + Typescript",
+      "T3 Stack with Drizzle, Clerk, UploadThing, Posthog. Next.js + Typescript",
   },
   {
-    id: 3,
+    id: 4,
     image: SIRVALO,
     title: "Sirvalo Games | Android App",
     github: "https://github.com/dansaround/hf-image-generator",
     demo: "https://www.instagram.com/sirvalo.games/",
     description:
       "Fullstack app with Node, React Native and Firebase | Repos are Private: links will redirect to customer's site. Typescript",
+  },
+  {
+    id: 5,
+    image: ANIME2WATCH,
+    title: "Responsive Web | Next.js",
+    github: "https://github.com/dansaround/anime2watch",
+    demo: "https://anime2watch.vercel.app/",
+    description:
+      "Used server components and integrated Apollo GraphQL Anilist API. Next.j + Typescript",
   },
 ];
 
